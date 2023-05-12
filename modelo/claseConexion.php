b0fbd87fb8d8e7138a8bd76cfd3b9e878df1d902
Alexis Tamayo <100908452+CGTamayo12@users.noreply.github.com>
2023-05-12 01:21:47 -0600

    Add files via upload

--- modelo/claseConexion.php
+++ modelo/claseConexion.php
@@ -3,10 +3,10 @@
 class Conexion extends \mysqli
 {
     public function getConexion(){
-        $host = "sql310.epizy.com";
-        $bdd = "epiz_33902531_lacuponera";
-        $user = "epiz_33902531";
-        $pass = "HymCR7f2nsns2z";
+        $host = "localhost";
+        $bdd = "proyecto_cuponera";
+        $user = "root";
+        $pass = "";
         try{
             $dsn = "mysql:host=$host;dbname=$bdd;charset=utf8";
             $dbh = new PDO($dsn,$user,$pass);
